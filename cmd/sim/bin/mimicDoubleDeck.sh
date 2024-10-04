96753f2b5c3da9db3005cd6f152e0adcec05fa5f
wade-rees-me <134008725+wade-rees-me@users.noreply.github.com>
2024-10-03 20:05:19 -0600

    Clean up

--- cmd/sim/bin/mimicDoubleDeck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NUMBER_OF_ROUNDS=10000000
-NUMBER_OF_ROUNDS=$(cd $(dirname "$0"); RETURN_VALUE=$(source ./runSimulation.sh $1 $NUMBER_OF_ROUNDS); echo $RETURN_VALUE)
-
-clear
-go run . -number-of-rounds $NUMBER_OF_ROUNDS -number-of-tables 1 -strategy-mimic -table-double-deck -table-penetration 0.75 -table-blackjack-pays 3:2
-
--- cmd/sim/bin/wongDoubleDeck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NUMBER_OF_ROUNDS=25000
-NUMBER_OF_ROUNDS=$(cd $(dirname "$0"); RETURN_VALUE=$(source ./runSimulation.sh $1 $NUMBER_OF_ROUNDS); echo $RETURN_VALUE)
-
-go run . -number-of-rounds $NUMBER_OF_ROUNDS -number-of-tables 1 -strategy-wong -table-double-deck -table-penetration 0.75 -table-blackjack-pays 3:2
-
