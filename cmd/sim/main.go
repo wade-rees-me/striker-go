d5222ccb46fe800a17d0474510b7365ae486d502
WADE  REES <134008725+wade-rees-me@users.noreply.github.com>
2025-05-24 15:35:54 -0600

    add remote logging

--- cmd/sim/main.go
+++ cmd/sim/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"runtime"
 	"sync"
 	"time"
@@ -10,9 +11,15 @@ import (
 	"github.com/wade-rees-me/striker-go/cmd/sim/constants"
 	"github.com/wade-rees-me/striker-go/cmd/sim/simulator"
 	"github.com/wade-rees-me/striker-go/cmd/sim/table"
+	"github.com/wade-rees-me/striker-go/cmd/sim/xlog"
 )
 
 func main() {
+	err := xlog.InitSyslog(xlog.SyslogAddress, xlog.SyslogPort)
+	if err != nil {
+		log.Fatalf("init_syslog failed: %v", err)
+	}
+	defer xlog.CloseSyslog()
 	var wg sync.WaitGroup
 	args := arguments.NewArguments()
 	params := arguments.NewParameters(args)
@@ -22,6 +29,10 @@ func main() {
 	finalReport := new(arguments.Report)
 
 	runtime.GOMAXPROCS(runtime.NumCPU()) // Utilize all available CPUs
+
+	xlog.LogInfo("Simulation started at %v", time.Now())
+	xlog.LogError("Encountered an error: %s", "bad luck")
+	xlog.LogFatal("Simulation aborted: %s", "fatal error")
 
 	fmt.Printf("Start: %s ...\n", constants.StrikerWhoAmI)
 	fmt.Printf("  -- arguments -------------------------------------------------------------------\n")
